1978f7762f4faff449f40b46bd866f4d581f443d
xdaTq <70078893+xdaTq@users.noreply.github.com>
2021-05-27 10:42:48 +0200

    fix(cart): Fixed cart bug & Typos in code.

--- commands/cart.js
+++ commands/cart.js
@@ -1,7 +1,9 @@
 const discord = require('discord.js');
 const axios = require('axios');
 const config = require('../config.json');
-const { chownSync } = require('fs');
+const {
+    chownSync
+} = require('fs');
 
 module.exports = {
     name: 'cart',
@@ -21,59 +23,52 @@ module.exports = {
             swapgg = response.data
         } catch (error) {
             return message.channel.send(`***${args[0]}*** doesn't exist, or data isn't being collected`),
-            console.log(error)
+                console.log(error)
         }
 
         let items = swapgg.result.items
-        console.log(items)
+        // console.log(items)
 
         for (let i = 0; i < items.length; i++) {
 
             let current = items[i];
             let oldPrice = current.sale.price
-            let comaprePrice = 10000
             let old = oldPrice.toString();
-    
+            let comaprePrice = 10000
+
             if (oldPrice >= comaprePrice) {
-                let newPrice = old.slice(',', 3);
+                let Testprice = `${old.substring(0, 3)},${old.substring(3, 5)}$`
 
-            } else if (oldPrice <= comaprePrice) {
-                let newPrice2 = old.slice(',', 2);
+                console.log(Testprice)
+            } else if (oldPrice < comaprePrice) {
+                let Testprice1 = `${old.substring(0, 3)},${old.substring(3, 5)}$`
 
+                console.log(Testprice1);
             }
 
-
-            console.log(testfunc)
-            
             const swapggembed = new discord.MessageEmbed()
                 .setTitle('Swap.gg')
                 .setColor(0x15CCB5)
-                .addFields(
-                {
+                .addFields({
                     name: 'Item Name: ',
                     value: current.marketName
-                },
-                {
+                }, {
                     name: 'Item type: ',
                     value: current.meta.TYPE
-                },
-                {
+                }, {
                     name: 'Item Category: ',
                     value: current.meta.CATEGORY
-                },
-                {
+                }, {
                     name: 'Item Price: ',
                     value: `${current.sale.price}$`
-                },
-                {
+                }, {
                     name: 'Steam Price: ',
                     value: `${current.prices.steam}$`
-                }
-                )
-    
+                })
+
             message.channel.send(swapggembed)
-            
-            
+
+
         }
 
 
